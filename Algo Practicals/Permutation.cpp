06c0ca412f7f5882ab23f05139ef1fcd1c0cab4c
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2023-01-03 01:46:13 +0530

    Few data mining practicals

---
+++ Algo Practicals/Permutation.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void display(int a[], int n)
+{
+	for (int i = 0; i < n; i++) {
+	cout << a[i] << " ";
+}
+cout << endl;
+}
+
+// Function to find the permutations
+void findPermutations(int a[], int n)
+{
+
+ // Sort the given array
+ sort(a, a + n);
+ 
+ // Find all possible permutations
+ cout << "Possible permutations are:\n";
+ do {
+  display(a, n);
+ } while (next_permutation(a, a + n));
+}
+
+// Driver code
+int main()
+{
+ int a[] = { 1, 2, 3, 4 };
+ 
+ int n = sizeof(a) / sizeof(a[0]);
+ 
+ findPermutations(a, n);
+ 
+ return 0;
+}
