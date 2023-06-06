1805316e3a6e89bf34b072074b7e39ae9ad2a36f
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2023-06-06 12:18:53 +0530

    adding the stuffs for os

--- Java/Demo1.java
+++ Java/Demo1.java
@@ -3,6 +3,10 @@ import p1.ConstructorExample;
 
 public class Demo1 extends ConstructorExample{
 
+	public int sum(int a, int b){
+		System.out.println("The sum of two numbers will be: "+(a+b));
+	}
+
 	public static void main(String[] args) {
 
 		ConstructorExample obj1 = new ConstructorExample();
