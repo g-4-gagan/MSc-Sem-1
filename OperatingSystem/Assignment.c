1662f384e61dee3d231af068b9756fbf7055ce9d
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2023-07-05 04:55:00 +0530

    few functions and menu driven program

--- OperatingSystem/Assignment.c
+++ OperatingSystem/Assignment.c
@@ -21,11 +21,82 @@ NOTE:
 #include<fcntl.h>
 #include<unistd.h>
 #include<stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char const *argv[])
 {
-	int testInteger;
-	printf("%s\n", "Enter a number:");
-	scanf("%d", &testInteger); 
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stdin, NULL, _IONBF, 0);
+	printf("This is a Menu-Driven File Operations Simulation Program\n");
+	bool main_flag = true;
+	while(main_flag)
+	{
+		printf("File Operation Main Menu: \n");
+		printf("1. Create a file\n");
+		printf("2. Open a file\n");
+		printf("3. other operations\n");
+		printf("4. Exit\n");
+
+		int choice;
+		scanf("%d", &choice);
+		if(choice==4){
+			// main_flag = false;
+			break;
+		}
+		if(choice<1 || choice>4)
+		{
+			printf("Invalid choice, Enter again\n");
+		}
+		else{
+			switch(choice){
+				case 1: printf("Creating file Menu:\n");
+						char* file_name;
+						char perm[9];
+						printf("Enter file name:");
+						scanf("%s",file_name);
+						printf("Enter file permission in format 'rwxrwxrwx(USR-GRP-OTH)':\n");
+						scanf("%s", perm);
+						int fd = 12;
+						if(fd!=-1){
+							bool int_flag = true;
+							printf("File created Successfully and opened in write mode: \n");
+							while(int_flag){
+								printf("1. Write to created file\n");
+								printf("2. Close the file and return to main menu\n");
+								printf("3. other operations\n");
+								printf("4. Exit the simulation\n");
+								int int_choice;
+								scanf("%d", &int_choice);
+								if(int_choice==4){
+									main_flag = false;
+									break;
+								}
+								if(int_choice<1 || int_choice>4)
+								{
+									printf("Invalid choice, Enter again\n");
+								}
+								else{
+									switch(int_choice){
+										case 1: printf("Write to file Menu:\n");
+												break;
+										case 2: printf("closing file and return to main menu\n");
+												int_flag = false;
+												break;
+										case 3: printf("Other operations:\n");
+												break;		
+									}
+								}
+							}
+						}
+						
+						break;
+				case 2: printf("Opening file Menu:\n");
+						break;
+				case 3: printf("other operation file Menu:\n");
+						break;
+			}
+		}
+	}
+	printf("Thanks for using the Simulation");
 	return 0;
 }
--- OperatingSystem/filestream.c
+++ OperatingSystem/filestream.c
@@ -2,39 +2,74 @@
 #include<unistd.h>
 #include<stdio.h>
 
-int createFile(char* path, mode_t mode){
-	return creat(path, mode);
+int createFile(char* path, char* perm){
+	// const char *perm = "rwxrwxrwx";
+	// mode_t mode = 0;
+
+	// if (perm[0] == 'r')
+	//   mode |= 0400;
+	// if (perm[1] == 'w')
+	//   mode |= 0200;
+	// if (perm[2] == 'x')
+	//   mode |= 0100;
+	// if (perm[3] == 'r')
+	//   mode |= 0040;
+	// if (perm[4] == 'w')
+	//   mode |= 0020;
+	// if (perm[5] == 'x')
+	//   mode |= 0010;
+	// if (perm[6] == 'r')
+	//   mode |= 0004;
+	// if (perm[7] == 'w')
+	//   mode |= 0002;
+	// if (perm[8] == 'x')
+	//   mode |= 0001;
+	mode_t mode = (((perm[0] == 'r') * 4 | (perm[1] == 'w') * 2 | (perm[2] == 'x')) << 6) |
+              (((perm[3] == 'r') * 4 | (perm[4] == 'w') * 2 | (perm[5] == 'x')) << 3) |
+              (((perm[6] == 'r') * 4 | (perm[7] == 'w') * 2 | (perm[8] == 'x')));
+    int fd = creat(path, mode);
+    if(fd == -1){
+    	printf("Error while creating the File\n");
+    }
+	return fd;
 }
+
+int openFile(char* path, int mode){
+	int fd;
+
+	switch(mode){
+		case 0: fd = open(path, O_RDONLY);
+				break;
+		case 1: fd = open(path, O_WRONLY);
+				break;
+		case 2: fd = open(path, O_RDWR);
+				break; 
+	}
+
+	if(fd == -1){
+		printf("Error while opening file check for file with given path and its permissions");
+	}
+
+	return fd;
+}
+
+int closeFile(int fd){
+	int res = close(fd);
+	if(res == -1){
+		printf("Error while closing the file");
+	}
+	
+	return res;
+}
+
 
 int main(int argc, char const *argv[])
 {
 	int testInteger;
 	printf("%s\n", "Enter a number:");
+	setvbuf(stdout, NULL, _IOLBF, 0);
 	scanf("%d", &testInteger);
-	int a = createFile("abc.txt", 0666);
+	int a = createFile("abc.txt", "rw-rw-rw-");
 	printf("%d %d\n", testInteger, &a);
 	return 0;
 }
-
-
-// const char *perm = "rwxrwxrwx";
-// mode_t mode = 0;
-
-// if (perm[0] == 'r')
-//   mode |= 0400;
-// if (perm[1] == 'w')
-//   mode |= 0200;
-// if (perm[2] == 'x')
-//   mode |= 0100;
-// if (perm[3] == 'r')
-//   mode |= 0040;
-// if (perm[4] == 'w')
-//   mode |= 0020;
-// if (perm[5] == 'x')
-//   mode |= 0010;
-// if (perm[6] == 'r')
-//   mode |= 0004;
-// if (perm[7] == 'w')
-//   mode |= 0002;
-// if (perm[8] == 'x')
-//   mode |= 0001;
